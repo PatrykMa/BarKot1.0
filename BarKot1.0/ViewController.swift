bf11f8643af534293c805b4b8df768ca626ec903
RMS2018 <rms2018@MacAir0.local>
2019-03-01 15:08:29 +0100

    Initial Commit

---
+++ BarKot1.0/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BarKot1.0
+//
+//  Created by RMS2018 on 01/03/2019.
+//  Copyright © 2019 M&M. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
